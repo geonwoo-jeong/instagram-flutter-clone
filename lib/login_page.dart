033cda90c489ddf88fea7fd32f621e60e9a3aa40
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-02-03 22:22:25 +0900

    Firebase and Google Auth

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatelessWidget {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,11 +26,28 @@ class LoginPage extends StatelessWidget {
             ),
             SignInButton(
               Buttons.Google,
-              onPressed: () {},
+              onPressed: () {
+                _handleSignIn().then((user) {
+                  print(user);
+                });
+              },
             )
           ],
         ),
       )
     );
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser user = (await _auth.signInWithCredential(
+      GoogleAuthProvider.getCredential(
+        idToken: googleAuth.idToken,
+        accessToken: googleAuth.accessToken
+      )
+    )).user;
+
+    return user;
   }
 }
